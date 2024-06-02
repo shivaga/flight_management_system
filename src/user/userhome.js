940e6837db3eee96b401e9b19b55612c4d70955c
shivaga <ushaa4871@gmail.com>
2024-06-03 00:25:00 +0530

    Frontend user added

--- src/user/userhome.js
+++ src/user/userhome.js
@@ -1,22 +1,38 @@
 import React from "react"
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+import Navbar from '../Frontend/Navbar/Navbar.jsx'
+import Home from '../Frontend/Home/Home.jsx'
+import Search from '../Frontend/Search/Search.jsx'
+import Support from '../Frontend/Support/Support.jsx'
+import Info from '../Frontend/Info/Info.jsx'
+import Lounge from '../Frontend/Lounge/Lounge.jsx'
+import Travelers from '../Frontend/Travelers/Travelers.jsx'
+import Subscribe from '../Frontend/Subscribers/Subscribe.jsx'
+import Footer from '../Frontend/Footer/Footer.jsx'
+import '../App.css';
 
-function Userhome (){
-    const history=useNavigate();
-    const img_prop={
-        maxWidth:'400px',
-        textAlign:'center'
-    }
-    function handlelogout(){
-        localStorage.clear();
-        history('/');
-    }
+
+function Userhome() {
+    // const history = useNavigate();
+    // const img_prop = {
+    //     maxWidth: '400px',
+    //     textAlign: 'center'
+    // }
+    // function handlelogout() {
+    //     localStorage.clear();
+    //     history('/');
+    // }
     return (
-        <div className="user_homepage">
-            <h1>Hello user and welcome to the home</h1>
-            <img style={img_prop} src="https://static.vecteezy.com/system/resources/previews/029/269/933/original/i-am-currently-under-construction-vector.jpg"
-            alt="Site construction warning"></img>
-            <button onClick={()=>handlelogout()}>Logout</button>
+        <div className="App">
+            <Navbar />
+            <Home />
+            <Search />
+            <Support />
+            <Info />
+            <Lounge />
+            <Travelers />
+            <Subscribe />
+            <Footer />
         </div>
     )
 }
